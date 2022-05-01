2ba883478b22f9163221b3eb2959b9d8cafdb217
jdRanda <randa.jtd@gmail.com>
2022-05-01 12:25:18 +0100

    Maybe fixed loss logging

--- DQN/dqntrain.py
+++ DQN/dqntrain.py
@@ -2,7 +2,6 @@ import gym
 from dqnagent import Agent
 import matplotlib.pyplot as plt
 import numpy as np
-import time
 from torch.utils.tensorboard import SummaryWriter
 
 def trainAnAgent(agent, displayScores=False, clip=False, render=False, save=False, nEpisodes = 270):
@@ -10,12 +9,13 @@ def trainAnAgent(agent, displayScores=False, clip=False, render=False, save=Fals
     env = gym.make('LunarLander-v2')
     scores = []
     epLengths  = []
+    losses = []
     animation = "|/-\\"
-    startTime = time.process_time()
 
     for i in range(nEpisodes):    
         score = 0
         epLen = 0
+        runningLoss = 0.0
         terminal = False
         observation = env.reset()
         while not terminal:
@@ -27,17 +27,21 @@ def trainAnAgent(agent, displayScores=False, clip=False, render=False, save=Fals
             score += reward
             agent.replayMemory.storeTransition(observation, action, reward, observation_, terminal)
             loss = agent.learn()
+            if loss is not None:
+                runningLoss += loss
             observation = observation_
             epLen += 1 
         
         scores.append(score)
         epLengths.append(epLen)
+        losses.append(runningLoss)
         avgScore = np.mean(scores[-100:])
         avgEpLen = np.mean(epLengths[-100:])
+        avgLoss = np.mean(losses[-100:])
 
         writer.add_scalar("avg_reward",avgScore,i)
         writer.add_scalar("avg_len",avgEpLen,i)
-        writer.add_scalar("loss",loss,i)
+        writer.add_scalar("loss",avgLoss,i)
 
         if displayScores:
             print(f"epsiode {i}, score {score:.2f}, avg score {avgScore:.2f}, epsilon {agent.epsilon:.3f}")
@@ -48,9 +52,7 @@ def trainAnAgent(agent, displayScores=False, clip=False, render=False, save=Fals
             writer.close()
             break
 
-    runTime = time.process_time() - startTime
-    print(f"\n Training took {runTime:.2f} seconds for {nEpisodes} episodes.",
-          f"\n Transitions encountered: {agent.replayMemory.memCntr},",
+    print(f"\n {nEpisodes} episodes, {agent.replayMemory.memCntr} transitions encountered,",
           f" Last 100 avg: {avgScore:.2f}")
 
     if save:
@@ -58,18 +60,16 @@ def trainAnAgent(agent, displayScores=False, clip=False, render=False, save=Fals
         if filename != "no":
             agent.save(filename)
 
-    return runTime
 
 
 if __name__ == '__main__':
     nAgents = 1
     agentScores = []
     totalTime = 0
-    maxEpisodes = 400
+    maxEpisodes = 1000
 
-    clip=nAgents==1
+    clip=True
     saveAgent=False
-    saveScores=False
     render=False
     displayScores=False
 
@@ -81,21 +81,6 @@ if __name__ == '__main__':
     lr=.0005 # learning rate
     c=4 # target network update rate
     k=1 # batch sampling rate
-
-
-    # lrs = [.0001, .0005, .00025]
-
-    # for lr in lrs:
-    #     for _ in range(nAgents):
-    #         agent = Agent(gamma=gamma, epsilon=epsilon, epsMin=epsMin, epsDecr=epsDecr,
-    #             batchSize=batchSize, nActions=4, inputDims=[8], lr=lr, c=c, k=k)
-
-    #         scores, runTime = trainAnAgent(agent, clip=clip, render=render, 
-    #             displayScores=displayScores, save=saveAgent, nEpisodes=maxEpisodes)
-            
-    #         totalTime += runTime
-    #         agentScores.append(scores)
-    #     writeScores(avgRewards(agentScores), filename=f"lrIS{lr}")
 
     for _ in range(nAgents):
         agent = Agent(gamma=gamma, epsilon=epsilon, epsMin=epsMin, epsDecr=epsDecr,
